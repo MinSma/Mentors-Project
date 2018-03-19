c7cf1574a5c8010af4a070ee08360735249bded9
kliusas <k.kliucnikas@gmail.com>
2018-03-19 22:24:54 +0200

    Custom Request Added

---
+++ app/Http/Requests/MentorUpdateRequest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MentorUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email'            => 'required|email',
+            'first_name'       => 'required',
+            'last_name'        => 'required',
+            'gender'           => 'required',
+            'age'              => 'required|min:value',
+            'city'             => 'required',
+            'topic'            => 'required',
+            'fixed_hour_price' => 'required',
+        ];
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getFirstName()
+    {
+        return $this->input('first_name');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getLastName()
+    {
+        return $this->input('last_name');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getEmail()
+    {
+        return $this->input('email');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getGender()
+    {
+        return $this->input('gender');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getAge()
+    {
+        return $this->input('age');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getCity()
+    {
+        return $this->input('city');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getTopic()
+    {
+        return $this->input('topic');
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getFixedHourPrice()
+    {
+        return $this->input('fixed_hour_price');
+    }
+
+}
