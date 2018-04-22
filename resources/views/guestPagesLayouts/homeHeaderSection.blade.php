debbd4d361af473cb9c670b7584318843d0efc62
kliusas <k.kliucnikas@gmail.com>
2018-04-22 21:41:36 +0300

    search form, students registration and login form added

--- resources/views/guestPagesLayouts/homeHeaderSection.blade.php
+++ resources/views/guestPagesLayouts/homeHeaderSection.blade.php
@@ -1,14 +1,29 @@
 <div class="jumbotron text-center">
     <div class="container-fluid">
-        <img style="max-width:80%; margin-top: -100px; opacity: 1" src="{{ asset('images/logo_black.png') }}">
+        <img style="max-width:40%; margin-top: -100px; opacity: 1" src="{{ asset('images/logo_black.png') }}">
         <p>Mokytis daug lengviau, kai šalia patikimas mokytojas!</p>
-    </div>
-    <form>
-        <div class="input-group" style="margin-top: -50px">
-            <input type="Subject" class="form-control" size="50" placeholder="Dalykinė sritis" required>
-            <div class="input-group-btn">
-                <button type="button" class="btn btn-danger">Ieškoti mentorių</button>
-            </div>
+
+    <form action="/mentors/found" method="POST" role="search">
+        {{ csrf_field() }}
+
+        <div class="form-group">
+            <label for="topic">Pasirinkti temą</label>
+
+            <select name="topic" id="topic" class="form-control">
+                <option value="mathematics">Mathematics</option>
+                <option value="english">English language</option>
+                <option value="it">Information Technology</option>
+                <option value="chemistry">Chemistry</option>
+                <option value="physics">Physics</option>
+                <option value="biology">Biology</option>
+                <option value="geography">Geography</option>
+                <option value="history">History</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Ieškoti</button>
         </div>
     </form>
+    </div>
 </div>
