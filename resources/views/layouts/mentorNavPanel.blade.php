72c984222b4d8022e8cbed72ba555e5017bd04a7
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-16 21:46:50 +0300

    Added "dashboard" to Mentor and Student navigation

--- resources/views/layouts/mentorNavPanel.blade.php
+++ resources/views/layouts/mentorNavPanel.blade.php
@@ -1,2 +1,3 @@
+<li><a href="{{ route('mentors.dashboard') }}">Dashboard</a></li>
 <li><a href="{{ route('mentors.changePassword') }}">Change Password</a></li>
 <li><a href="{{ route('login.disconnect') }}">Logout</a></li>
--- resources/views/layouts/studentNavPanel.blade.php
+++ resources/views/layouts/studentNavPanel.blade.php
@@ -1,2 +1,3 @@
+<li><a href="{{ route('students.dashboard') }}">Dashboard</a></li>
 <li><a href="{{ route('students.changePassword') }}">Change Password</a></li>
 <li><a href="{{ route('login.disconnect') }}">Logout</a></li>
