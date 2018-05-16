9f225dd9f6714be22a7216d2c663d261f7fff49d
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-16 20:19:57 +0300

    Add strict types to ContactsController

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -1,18 +1,30 @@
 <?php
+declare(strict_types = 1);
 
 namespace App\Http\Controllers;
 
 use App\Notifications\InboxMessage;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\ContactFormRequest;
 use App\Admin;
 
+/**
+ * Class ContactsController
+ * @package App\Http\Controllers
+ */
 class ContactsController extends Controller
 {
-    public function show() {
+    /**
+     * @return View
+     */
+    public function show() : View{
         return view('contactus.index');
     }
 
+    /**
+     * @param ContactFormRequest $message
+     * @param Admin $admin
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function mailToAdmin(ContactFormRequest $message, Admin $admin) {
         $admin->notify(new InboxMessage($message));
 
