77392103514d634c4e715a2a2f609f7ee486af1a
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-16 21:07:37 +0300

    Add show views for Student and Mentor

---
+++ resources/views/mentors/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+@section('title', 'Users')
+@section('menu')
+    @include('layouts.mentorNavPanel')
+@endsection
+
+@section('content')
+
+    <h1>Mentor's information</h1>
+
+    <h3>First name:</h3><?php echo $mentor->first_name ?>
+    <h3>Last name:</h3><?php echo $mentor->last_name ?>
+    <h3>Email address:</h3><?php echo $mentor->email ?>
+    <h3>Gender:</h3><?php echo $mentor->gender ?>
+    <h3>Age:</h3><?php echo $mentor->age ?>
+    <h3>City:</h3><?php echo $mentor->city ?>
+    <h3>Topic:</h3><?php echo $mentor->topic ?>
+    <h3>Fixed hour price:</h3><?php echo $mentor->fixed_hour_price ?>
+
+@endsection
---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+@section('title', 'Users')
+@section('menu')
+    @include('layouts.studentNavPanel')
+@endsection
+
+@section('content')
+
+    <h1>Student's information</h1>
+
+    <h3>First name:</h3><?php echo $student->first_name ?>
+    <h3>Last name:</h3><?php echo $student->last_name ?>
+    <h3>Email address:</h3><?php echo $student->email ?>
+    <h3>Gender:</h3><?php echo $student->gender ?>
+    <h3>Age:</h3><?php echo $student->age ?>
+    <h3>City:</h3><?php echo $student->city ?>
+
+@endsection
