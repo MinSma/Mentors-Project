e966c2befc0f279fecc7ec3a3d67a0cc95677841
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-19 19:26:07 +0300

    Add view for students representation in mentor's dashboard

---
+++ resources/views/mentors/students.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.main')
+@section('title', 'Login Panel')
+@section('menu')
+    @include('layouts.mentorNavPanel')
+@endsection
+@section('content')
+    <h2>Students with belongs to you:</h2>
+
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <td>ID</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Gender</td>
+            <td>Age</td>
+            <td>City</td>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($mentor->students as $key => $value)
+            <tr>
+                <td>{{ $value->id }}</td>
+                <td>{{ $value->first_name }}</td>
+                <td>{{ $value->last_name }}</td>
+                <td>{{ $value->email }}</td>
+                <td>{{ $value->gender }}</td>
+                <td>{{ $value->age }}</td>
+                <td>{{ $value->city }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
