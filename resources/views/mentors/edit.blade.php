9ddcbb417d652752790b7097c243b9b8350e4091
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-21 14:24:40 +0300

    Fix mentors and students edit views to have old values in fields after failed form submition

--- resources/views/mentors/edit.blade.php
+++ resources/views/mentors/edit.blade.php
@@ -10,51 +10,49 @@
 
     <div class="form-group">
         {!! Form::label('first_name', 'First Name', ['class' => 'control-label']) !!}
-        {!! Form::text('first_name', null, ['class' => 'form-control']) !!}
+        {!! Form::text('first_name', old('first_name'), ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('last_name', 'Last Name', ['class' => 'control-label']) !!}
-        {!! Form::text('last_name', null, ['class' => 'form-control']) !!}
+        {!! Form::text('last_name', old('last_name'), ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('gender', 'Gender:', ['class' => 'control-label']) !!}
-        {!! Form::text('gender', null, ['class' => 'form-control']) !!}
+        {!! Form::select('gender', [
+            'man' => 'Man',
+            'woman' => 'Woman'
+        ], old('topic'), ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('age', 'Age:', ['class' => 'control-label']) !!}
-        {!! Form::text('age', null, ['class' => 'form-control']) !!}
+        {!! Form::text('age', old('age'), ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('city', 'City:', ['class' => 'control-label']) !!}
-        {!! Form::text('city', null, ['class' => 'form-control']) !!}
+        {!! Form::text('city', old('city'), ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('topic', 'Topic:', ['class' => 'control-label']) !!}
         {!! Form::select('topic', [
-        'mathematics' => 'Mathematics',
-        'english' => 'English language',
-        'it' => 'Information Technology',
-        'chemistry' => 'Chemistry',
-        'physics' => 'Physics',
-        'biology' => 'Biology',
-        'geography' => 'Geography',
-        'history' => 'History',
-        ], null, ['class' => 'form-control']) !!}
+            'mathematics' => 'Mathematics',
+            'english' => 'English language',
+            'it' => 'Information Technology',
+            'chemistry' => 'Chemistry',
+            'physics' => 'Physics',
+            'biology' => 'Biology',
+            'geography' => 'Geography',
+            'history' => 'History'
+        ], old('topic'), ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('fixed_hour_price', 'Fixed Hour Price:', ['class' => 'control-label']) !!}
-        {!! Form::text('fixed_hour_price', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('password', 'Password:', ['class' => 'control-label']) !!}
-        {!! Form::password('password',  null, ['class' => 'form-control']) !!}
+        {!! Form::text('fixed_hour_price', old('fixed_hour_price'), ['class' => 'form-control']) !!}
     </div>
 
     {!! Form::hidden('id', $mentor->id) !!}
