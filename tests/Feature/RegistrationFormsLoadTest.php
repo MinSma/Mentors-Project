a79fe51bf3271cd063944e701b5d7f4fe206d512
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-26 21:42:25 +0300

    Fix tests

--- tests/Feature/RegistrationFormsLoadTest.php
+++ tests/Feature/RegistrationFormsLoadTest.php
@@ -8,11 +8,11 @@ class RegistrationFormsLoadTest extends TestCase
 {
     public function testDoesMentorRegistrationFormLoads()
     {
-        $this->get('/mentors/create')->assertSee('Register');
+        $this->get('/mentors/create')->assertSee('Registruotis');
     }
 
     public function testDoesStudentRegistrationFormLoads()
     {
-        $this->get('/students/create')->assertSee('Register');
+        $this->get('/students/create')->assertSee('Registruotis');
     }
 }
