50bed5373decc8881aa717e8974a989584f6db81
Mindaugas Smalinskas <mindaugas1017@gmail.com>
2018-05-26 20:14:03 +0300

    Fix dashboards/students/mentors views

--- resources/views/mentors/students.blade.php
+++ resources/views/mentors/students.blade.php
@@ -1,35 +1,41 @@
-@extends('layouts.main')
-@section('title', 'Login Panel')
-@section('menu')
-    @include('layouts.NavPanel')
-@endsection
-@section('content')
-    <h2>Studentai, kurie priklauso Jums:</h2>
+@section('title', 'Students')
 
-    <table class="table table-striped table-bordered">
-        <thead>
-        <tr>
-            <td>ID</td>
-            <td>Vardas</td>
-            <td>Pavardė</td>
-            <td>Elektroninio pašto adresas</td>
-            <td>Lytis</td>
-            <td>Amžius</td>
-            <td>Miestas</td>
-        </tr>
-        </thead>
-        <tbody>
+@include('guestPagesLayouts.homeHeaderIncludes')
+
+@include('layouts.NavPanel')
+@include('guestPagesLayouts.homeHeaderSection')
+
+<h1 class="mb-2 text-center">Studentai, kurie priklauso Jums:</h1>
+<br />
+<div class="container lower">
+    <div class="row">
         @foreach($students as $key => $value)
-            <tr>
-                <td>{{ $value->id }}</td>
-                <td>{{ $value->first_name }}</td>
-                <td>{{ $value->last_name }}</td>
-                <td>{{ $value->email }}</td>
-                <td>{{ $value->gender }}</td>
-                <td>{{ $value->age }}</td>
-                <td>{{ $value->city }}</td>
-            </tr>
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-xs-4 col-sm-4 col-md-5">
+                            <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" />
+                        </div>
+                        <div class="col-xs-8 col-sm-8 col-md-7">
+                            <h4 class="found-title">
+                                {{ $value->name }}</h4>
+                            <p>
+                                <i class="glyphicon glyphicon-user"></i>ID: {{ $value->id }}
+                                <br />
+                                <i class="glyphicon glyphicon-envelope"></i>{{ $value->email }}
+                                <br />
+                                <i class="glyphicon glyphicon-heart"></i>Lytis: {{ $value->gender }}
+                                <br />
+                                <i class="glyphicon glyphicon-eye-open"></i>Amžius: {{ $value->age }}
+                                <br />
+                                <i class="glyphicon glyphicon-globe"></i>{{ $value->city }}
+                                <br />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         @endforeach
-        </tbody>
-    </table>
-@endsection
+        {{--{{ $students->links() }}--}}
+    </div>
+</div>
--- resources/views/students/mentors.blade.php
+++ resources/views/students/mentors.blade.php
@@ -1,41 +1,47 @@
-@extends('layouts.main')
-@section('title', 'Login Panel')
-@section('menu')
-    @include('layouts.NavPanel')
-@endsection
-@section('content')
-    <h2>Mentoriai, kuriems priklausote:</h2>
+@section('title', 'Mentoriai')
+@include('guestPagesLayouts.homeHeaderIncludes')
 
-    <table class="table table-striped table-bordered">
-        <thead>
-        <tr>
-            <td>ID</td>
-            <td>Vardas</td>
-            <td>Pavardė</td>
-            <td>Elektroninio pašto adresas</td>
-            <td>Lytis</td>
-            <td>Amžius</td>
-            <td>Miestas</td>
-            <td>Tema</td>
-            <td>Valandinis įkainis</td>
-        </tr>
-        </thead>
+@include('layouts.NavPanel')
+@include('guestPagesLayouts.homeHeaderSection')
+
+
+<h1 class="mb-2 text-center">Mentoriai, kuriems priklausote:</h1>
+<br />
+<div class="container lower">
+    <div class="row">
         @if($mentors != NULL)
-            <tbody>
-                @foreach($mentors as $key => $value)
-                    <tr>
-                        <td>{{ $value->id }}</td>
-                        <td>{{ $value->first_name }}</td>
-                        <td>{{ $value->last_name }}</td>
-                        <td>{{ $value->email }}</td>
-                        <td>{{ $value->gender }}</td>
-                        <td>{{ $value->age }}</td>
-                        <td>{{ $value->city }}</td>
-                        <td>{{ $value->topic }}</td>
-                        <td>{{ $value->fixed_hour_price }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
+            @foreach($mentors as $key => $value)
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="well well-sm">
+                        <div class="row">
+                            <div class="col-xs-4 col-sm-4 col-md-5">
+                                <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" />
+                            </div>
+                            <div class="col-xs-8 col-sm-8 col-md-7">
+                                <h4 class="found-title">
+                                    {{ $value['name'] }}</h4>
+                                <p>
+                                    <i class="glyphicon glyphicon-user"></i>ID: {{ $value['id'] }}
+                                    <br />
+                                    <i class="glyphicon glyphicon-envelope"></i>{{ $value['email'] }}
+                                    <br />
+                                    <i class="glyphicon glyphicon-heart"></i>Lytis: {{ $value['gender'] }}
+                                    <br />
+                                    <i class="glyphicon glyphicon-eye-open"></i>Amžius: {{ $value['age'] }}
+                                    <br />
+                                    <i class="glyphicon glyphicon-globe"></i>{{ $value['city'] }}
+                                    <br />
+                                    <i class="glyphicon glyphicon-book"></i>{{ $value['topic'] }}
+                                    <br />
+                                    <i class="glyphicon glyphicon-fire"></i>{{ $value['fixed_hour_price'] }} EU
+                                    <br />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            {{--{{ $mentors->links() }}--}}
         @endif
-    </table>
-@endsection
+    </div>
+</div>
